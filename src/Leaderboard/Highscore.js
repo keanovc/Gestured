50d09f4b75d038bec5b8f94efc135c357c8e73cd
{RSpaan} <{rubespaa@student.arteveldehs.be}>
2022-05-25 15:16:31 +0200

    Leaderboard

---
+++ src/Leaderboard/Highscore.js
@@ -0,0 +1,10 @@
+export const Highscore = () => {
+    return (
+        <div className="highscoreBox">
+            <div className="position"></div>
+            <div className="name"></div>
+            <div className="gamesCount"></div>
+            <div className="winsCount"></div>
+        </div>
+    )
+}
