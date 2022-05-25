e6e187679f983c345f93df26ccc28935b854c768
keanovc <91675323+keanovc@users.noreply.github.com>
2022-05-25 14:30:46 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from './Login/Login';
+import Register from './Register/Register';
+import Reset from './Reset/Reset';
+import Dashboard from './Dashboard/Dashboard';
+import Game from './Game/Game';
+
+function App() {
+  return (
+    <div className="app">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/reset" element={<Reset />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/game" element={<Game />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
