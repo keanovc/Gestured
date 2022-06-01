5163acd4cd02cc81fd5424cbaf238ece98a65c8e
Michael Reinhard <michael.reinhard@student.arteveldehs.be>
2022-06-01 09:21:22 +0200

    Fixed menu toggle

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -25,18 +25,25 @@ const Navbar = () => {
       fetchUserName();
     }, [user, loading]);
 
+    const [open, setOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setOpen(!open);
+    };
+
+
 
   return (
     <nav class="flex items-center justify-between flex-wrap bg-gray-800 p-6 text-white">
     <div class="flex items-center flex-no-shrink text-white mr-6">
       <span class="font-semibold text-xl tracking-tight">Gestured</span>
     </div>
-    <div class="block lg:hidden">
-      <button class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
+    <div class=''>
+      <button onClick={toggleMenu} class="flex items-center px-3 py-2 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white">
         <svg class="h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
       </button>
     </div>
-    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class={`w-full block flex-grow lg:flex lg:items-center lg:w-auto ${ !open ? 'hidden' : ''}`}>
       <div class="text-sm lg:flex-grow">
         <div class="block mt-4 lg:inline-block lg:mt-0 text-teal-lighter hover:text-white mr-4">
           <Link to={'/'}>Home</Link>
