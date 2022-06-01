e55c86e501585499e655664756c19cbe48afb5bb
{RSpaan} <{rubespaa@student.arteveldehs.be}>
2022-06-01 10:19:09 +0200

    geen yarn

--- src/Leaderboard/Highscore.js
+++ src/Leaderboard/Highscore.js
@@ -1,10 +1,11 @@
 export const Highscore = () => {
     return (
-        <div className="highscoreBox">
-            <div className="position"></div>
-            <div className="name"></div>
-            <div className="gamesCount"></div>
-            <div className="winsCount"></div>
-        </div>
+        <tr class="bg-black bg-opacity-80 backdrop-filter backdrop-blur-lg border-b ">
+            <td class="px-4 py-3">Jill</td>
+            <td class="px-4 py-3">Smith</td>
+            <td class="px-4 py-3">50</td>
+            <td class="px-4 py-3">Male</td>
+            <td class="px-4 py-3">Male</td>
+        </tr>
     )
 }
--- src/Leaderboard/Leaderboard.js
+++ src/Leaderboard/Leaderboard.js
@@ -1,35 +1,51 @@
 import Navbar from "../Navbar/Navbar"
 
-export const Leaderboard = () => {
+export const Leaderboard = ({ children }) => {
     return (
         <>
             <Navbar />
-            <p class="text-lg text-center font-bold m-5">Leaderboard</p>
-            <table class="rounded-t-lg m-5 w-3/6 mx-auto bg-gray-800 text-gray-200">
-                <tr class="text-left border-b border-gray-300">
-                    <th class="px-4 py-3">Firstname</th>
-                    <th class="px-4 py-3">Lastname</th>
-                    <th class="px-4 py-3">Age</th>
-                    <th class="px-4 py-3">Sex</th>
-                </tr>
-                <tr class="bg-gray-700 border-b border-gray-600">
-                    <td class="px-4 py-3">Jill</td>
-                    <td class="px-4 py-3">Smith</td>
-                    <td class="px-4 py-3">50</td>
-                    <td class="px-4 py-3">Male</td>
-                </tr>
-                <tr class="bg-gray-700 border-b border-gray-600">
-                    <td class="px-4 py-3">Jill</td>
-                    <td class="px-4 py-3">Smith</td>
-                    <td class="px-4 py-3">50</td>
-                    <td class="px-4 py-3">Male</td>
-                </tr>
-                <tr class="bg-gray-700 border-b border-gray-600">
-                    <td class="px-4 py-3">Jill</td>
-                    <td class="px-4 py-3">Smith</td>
-                    <td class="px-4 py-3">50</td>
-                    <td class="px-4 py-3">Male</td>
-                </tr>
+            <p class="text-4xl text-center font-bold m-10 text-white">Leaderboard</p>
+            <table class="rounded-t-lg m-5 w-4/5 mx-auto bg-opacity-40 backdrop-filter backdrop-blur-lg bg-white text-gray-700 rounded-b-lg">
+                <thead>
+                    <tr class="text-left border-b border-gray-300 rounded-t-lg">
+                        <th class="px-4 py-3">#</th>
+                        <th class="px-4 py-3">Name</th>
+                        <th class="px-4 py-3">Games</th>
+                        <th class="px-4 py-3">Wins</th>
+                        <th class="px-4 py-3">Wins</th>
+                    </tr>
+                </thead>
+                <tbody className="text-gray-200">
+                    {children}
+                    <tr class="bg-black bg-opacity-80 backdrop-filter backdrop-blur-lg border-b ">
+                        <td class="px-4 py-3">Jill</td>
+                        <td class="px-4 py-3">Smith</td>
+                        <td class="px-4 py-3">50</td>
+                        <td class="px-4 py-3">Male</td>
+                        <td class="px-4 py-3">Male</td>
+                    </tr>
+                    <tr class="border-b bg-black bg-opacity-60 backdrop-filter backdrop-blur-lg">
+                        <td class="px-4 py-3">Jill</td>
+                        <td class="px-4 py-3">Smith</td>
+                        <td class="px-4 py-3">50</td>
+                        <td class="px-4 py-3">Male</td>
+                        <td class="px-4 py-3">Male</td>
+                    </tr>
+                    <tr class=" bg-black bg-opacity-80 backdrop-filter backdrop-blur-lg border-b ">
+                        <td class="px-4 py-3">Jill</td>
+                        <td class="px-4 py-3">Smith</td>
+                        <td class="px-4 py-3">50</td>
+                        <td class="px-4 py-3">Male</td>
+                        <td class="px-4 py-3">Male</td>
+                    </tr>
+                    <tr class="text-gray-700">
+                        <td class="py-3"></td>
+                        <td class="px-4 py-3"></td>
+                        <td class="px-4 py-3"></td>
+                        <td class="px-4 py-3"></td>
+                        <td class="px-4 py-3"></td>
+                    </tr>
+                </tbody>
             </table>
         </>
 
