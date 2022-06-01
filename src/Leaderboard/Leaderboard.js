89ebac6e68bd4c75ade74e41f212c4cc4f2713d3
{RSpaan} <{rubespaa@student.arteveldehs.be}>
2022-06-01 09:07:37 +0200

    update

--- src/Leaderboard/Leaderboard.js
+++ src/Leaderboard/Leaderboard.js
@@ -2,7 +2,7 @@ export const Leaderboard = () => {
     return (
         <div>
             <div id='leaderboard' className='flex column w-screen h-screen justify-center'>
-                <div id='header' className='flex row w-4/5  h-16 justify-evenly rounded-t-2xl items-center divide-x-8'>
+                <div id='header' className='flex row w-4/5  h-16 justify-evenly rounded-t-2xl items-center bg-midnight text-ivory'>
                     <div>Rank</div> <div>User</div> <div>Games played</div> <div>Games won</div> <div>win %</div>
                 </div>
             </div >
