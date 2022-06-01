b7cb28f4cf64826b0a2a078864a2f7fbdb931367
{RSpaan} <{rubespaa@student.arteveldehs.be}>
2022-06-01 09:36:39 +0200

    update

--- src/Leaderboard/Leaderboard.js
+++ src/Leaderboard/Leaderboard.js
@@ -1,11 +1,50 @@
 export const Leaderboard = () => {
     return (
-        <div>
-            <div id='leaderboard' className='flex column w-screen h-screen justify-center'>
-                <div id='header' className='flex row w-4/5  h-16 justify-evenly rounded-t-2xl items-center bg-midnight text-ivory'>
-                    <div>Rank</div> <div>User</div> <div>Games played</div> <div>Games won</div> <div>win %</div>
+        <div class="overflow-x-auto">
+            <div class="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center bg-gray-100 font-sans overflow-hidden">
+                <div class="w-full lg:w-5/6">
+                    <div class="bg-white shadow-md rounded my-6">
+                        <table class="min-w-max w-full table-auto">
+                            <thead>
+                                <tr class="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
+                                    <th class="py-3 px-6 text-left">Project</th>
+                                    <th class="py-3 px-6 text-left">Client</th>
+                                    <th class="py-3 px-6 text-center">Users</th>
+                                    <th class="py-3 px-6 text-center">Status</th>
+                                    <th class="py-3 px-6 text-center">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody class="text-gray-600 text-sm font-light">
+                                <tr class="border-b border-gray-200 hover:bg-gray-100">
+                                    <td class="py-3 px-6 text-left whitespace-nowrap">
+                                        <div class="flex items-center">
+                                            <span class="font-medium">React Project</span>
+                                        </div>
+                                    </td>
+                                    <td class="py-3 px-6 text-left">
+                                        <div class="flex items-center">
+                                            <span>Eshal Rosas</span>
+                                        </div>
+                                    </td>
+                                    <td class="py-3 px-6 text-center">
+                                        <div class="flex items-center justify-center">
+
+                                        </div>
+                                    </td>
+                                    <td class="py-3 px-6 text-center">
+                                        <span class="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-xs">Active</span>
+                                    </td>
+                                    <td class="py-3 px-6 text-center">
+                                        <div class="flex item-center justify-center">
+
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-            </div >
+            </div>
         </div>
 
     )
