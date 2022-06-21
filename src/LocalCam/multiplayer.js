a03036e2cb57b326aa2b49fac57d65d10bacde9d
RSpaan <rubespaa@student.arteveldehs.be>
2022-06-21 22:55:00 +0200

    multiplayer video data ok

--- src/LocalCam/multiplayer.js
+++ src/LocalCam/multiplayer.js
@@ -4,7 +4,7 @@ import { RulesButton } from "../Design/RulesButton/RulesButton";
 import { RulesModal } from "../Design/RulesModal/RulesModal";
 import { db } from "../firebase"
 import LoadingIndicator from "./LoadingIndicator";
-import { addDoc, collection, doc, setDoc } from "firebase/firestore";
+import { addDoc, collection, doc, getDoc, onSnapshot, setDoc, updateDoc } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyA4hhoiITIagcoUyxaGUYxlts6VZp2LL2A",
@@ -61,9 +61,9 @@ export default function Multiplayer() {
     let remoteStream = null;
 
     // HTML elements
-    const callInput = document.getElementById('callInput');
+    // const callInput = document.getElementById('callInput');
 
-    const hangupButton = document.getElementById('hangupButton');
+    // const hangupButton = document.getElementById('hangupButton');
 
     // 1. Setup media sources
 
@@ -74,15 +74,18 @@ export default function Multiplayer() {
 
         // Push tracks from local stream to peer connection
         localStream.getTracks().forEach((track) => {
+            console.log('tracks local', track);
             pc.addTrack(track, localStream);
         });
 
         // Pull tracks from remote stream, add to video stream
-        pc.ontrack = (event) => {
+        pc.addEventListener('track', (event) => {
+            console.log('onTRAICK');
             event.streams[0].getTracks().forEach((track) => {
+
                 remoteStream.addTrack(track);
             });
-        };
+        });
         const webcamVideo = document.getElementById('localFeed');
         webcamVideo.srcObject = localStream;
 
@@ -94,25 +97,26 @@ export default function Multiplayer() {
     // 2. Create an offer
     const createOffer = async () => {
         // Reference Firestore collections for signaling
-        // const callDoc = db.collection('calls').doc();
-        // const offerCandidates = callDoc.collection('offerCandidates');
-        // const answerCandidates = callDoc.collection('answerCandidates');
+        // const callDoc = collection(db, 'calls');
+        // const offerCandidates = collection(db, `calls/${callDoc.id}/offerCandidates`);
+        // const answerCandidates = collection(db, `calls/${callDoc.id}/answerCandidates`);
         const callDoc = await addDoc(collection(db, "calls"), {});
-        const offerCandidates = await addDoc(collection(callDoc, "offerCandidates"), {});
-        const answerCandidates = await addDoc(collection(callDoc, "anwserCandidates"), {});
-
+        const offerCandidates = collection(callDoc, "offerCandidates");
+        const answerCandidates = collection(callDoc, "anwserCandidates");
+        console.log(offerCandidates);
+        console.log(answerCandidates);
         setRoomCode(callDoc.id);
 
 
         // Get candidates for caller, save to db
         //event.candidate.toJSON()
         pc.onicecandidate = async (event) => {
-            const docRef = doc(db, "calls", callDoc.id, "offerCandidates", offerCandidates.id);
-            event.candidate && await addDoc(doc(docRef), event.candidate.toJSON());
+            console.log('enter onice local');
+            event.candidate && await addDoc(offerCandidates, event.candidate.toJSON());  
         };
 
         // Create offer
-        const offerDescription = await pc.createOffer();
+        const offerDescription = await pc.createOffer({ offerToReceiveAudio: true, offerToReceiveVideo: true });
         await pc.setLocalDescription(offerDescription);
 
         const offer = {
@@ -123,8 +127,11 @@ export default function Multiplayer() {
         await setDoc(doc(db, 'calls', callDoc.id), { offer })
 
         // Listen for remote answer
-        callDoc.onSnapshot((snapshot) => {
+        onSnapshot(callDoc, (snapshot) => {
+            console.log('foo', typeof(snapshot));
             const data = snapshot.data();
+
+            console.log('data for calldoc: ', data);
             if (!pc.currentRemoteDescription && data?.answer) {
                 const answerDescription = new RTCSessionDescription(data.answer);
                 pc.setRemoteDescription(answerDescription);
@@ -132,30 +139,32 @@ export default function Multiplayer() {
         });
 
         // When answered, add candidate to peer connection
-        answerCandidates.onSnapshot((snapshot) => {
+        onSnapshot(answerCandidates, (snapshot) => {
+            console.log('snpashot ', snapshot);
             snapshot.docChanges().forEach((change) => {
+                console.log('data for awnser: ', change);
                 if (change.type === 'added') {
                     const candidate = new RTCIceCandidate(change.doc.data());
                     pc.addIceCandidate(candidate);
                 }
             });
         });
-
-        hangupButton.disabled = false;
     };
 
     // 3. Answer the call with the unique ID
     const answer = async () => {
-        const callId = callInput.value;
-        const callDoc = db.collection('calls').doc(callId);
-        const answerCandidates = callDoc.collection('answerCandidates');
-        const offerCandidates = callDoc.collection('offerCandidates');
+        const callId = roomCode;
+        const callDoc = doc(db, 'calls', callId);
+        console.log(callDoc);
+        const offerCandidates = collection(callDoc, "offerCandidates");
+        const answerCandidates = collection(callDoc, "anwserCandidates");
 
-        pc.onicecandidate = (event) => {
-            event.candidate && answerCandidates.add(event.candidate.toJSON());
+        pc.onicecandidate = async (event) => {
+            console.log("blooblap");
+            event.candidate && await addDoc(answerCandidates, event.candidate.toJSON());
         };
 
-        const callData = (await callDoc.get()).data();
+        const callData = (await getDoc(callDoc)).data();
 
         const offerDescription = callData.offer;
         await pc.setRemoteDescription(new RTCSessionDescription(offerDescription));
@@ -168,9 +177,9 @@ export default function Multiplayer() {
             sdp: answerDescription.sdp,
         };
 
-        await callDoc.update({ answer });
+        await updateDoc(callDoc, { answer })
 
-        offerCandidates.onSnapshot((snapshot) => {
+        onSnapshot(offerCandidates, (snapshot) => {
             snapshot.docChanges().forEach((change) => {
                 console.log(change);
                 if (change.type === 'added') {
@@ -212,7 +221,7 @@ export default function Multiplayer() {
                         <div className="py-0 flex justify-center flex-col items-center gap-3">
                             <div className='rounded-full overflow-hidden w-[400px] h-[400px]'>
 
-                                {!remoteStream ? (
+                                {loading ? (
                                     <div className='bg-black w-[400px] h-[400px] flex justify-center items-center'>
                                         <LoadingIndicator
                                             width={200}
