0081c00ff8768af908acaf0881d9bd71e3c5acf0
{RSpaan} <{rubespaa@student.arteveldehs.be}>
2022-06-22 09:41:24 +0200

    m

--- src/Modes/Modes.js
+++ src/Modes/Modes.js
@@ -19,15 +19,15 @@ const Modes = () => {
                         <Link to={'/modes/localcam'}>
                             <div className="p-8 cursor-pointer rounded-3xl bg-white border transition duration-300 ease-in-out hover:scale-105 hover:drop-shadow-2xl">
                                 <div className="-mb-20 -translate-y-1/2 transform">
-                                <img src="../../img/robot1.png" alt="User vs AI" title="User vs AI" className="mx-auto h-48" />
+                                    <img src="../../img/robot1.png" alt="User vs AI" title="User vs AI" className="mx-auto h-48" />
                                 </div>
                                 <div className="text-center">
-                                <h3 className="text-center text-4xl font-bold">User VS AI</h3>
-                                <span className="text-sm">Webcam</span>
+                                    <h3 className="text-center text-4xl font-bold">User VS AI</h3>
+                                    <span className="text-sm">Webcam</span>
                                 </div>
                                 <h1 className="mt-16 mb-20 flex justify-center text-center text-2xl">Play your favorite game against the AI with your webcam!</h1>
                                 <div className="text-center">
-                                <button className="rounded-xl bg-black w-full py-2 text-white">Select gamemode</button>
+                                    <button className="rounded-xl bg-black w-full py-2 text-white">Select gamemode</button>
                                 </div>
                             </div>
                         </Link>
@@ -36,38 +36,38 @@ const Modes = () => {
                         <Link to={'/modes/localbuttons'}>
                             <div className="p-8 cursor-pointer rounded-3xl bg-white border transition duration-300 ease-in-out hover:scale-105 hover:drop-shadow-2xl">
                                 <div className="-mb-20 -translate-y-1/2 transform">
-                                <img src="../../img/user.png" alt="User VS AI" title="User VS AI" className="mx-auto h-48" />
+                                    <img src="../../img/user.png" alt="User VS AI" title="User VS AI" className="mx-auto h-48" />
                                 </div>
                                 <div className="text-center">
-                                <h3 className="text-center text-4xl font-bold">User VS AI</h3>
-                                <span className="text-sm">Buttons</span>
+                                    <h3 className="text-center text-4xl font-bold">User VS AI</h3>
+                                    <span className="text-sm">Buttons</span>
                                 </div>
-                                <h1 className="mt-16 mb-20 flex justify-center text-center text-2xl">Don't you have a webcam? Then play this mode with very easy!</h1>
+                                <h1 className="mt-16 mb-20 flex justify-center text-center text-2xl">Don't you have a webcam? No problem play with buttons in this gamemode!</h1>
                                 <div className="text-center">
-                                <button className="rounded-xl bg-black w-full py-2 text-white">Select gamemode</button>
+                                    <button className="rounded-xl bg-black w-full py-2 text-white">Select gamemode</button>
                                 </div>
                             </div>
                         </Link>
                     </div>
-                        <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
-                            <Link to={"/modes/multiplayer"}>
+                    <div className="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
+                        <Link to={"/modes/multiplayer"}>
                             <div className="p-8 cursor-pointer rounded-3xl bg-white border transition duration-300 ease-in-out hover:scale-105 hover:drop-shadow-2xl">
                                 <div className="-mb-20 -translate-y-1/2 transform">
-                                <img src="../../img/wifi.png" alt="User vs User" title="User vs User" className="mx-auto h-48" />
+                                    <img src="../../img/wifi.png" alt="User vs User" title="User vs User" className="mx-auto h-48" />
                                 </div>
                                 <div className="text-center">
-                                <h3 className="text-center text-4xl font-bold">User VS User</h3>
-                                <span className="text-sm">Online</span>
+                                    <h3 className="text-center text-4xl font-bold">User VS User</h3>
+                                    <span className="text-sm">Online</span>
                                 </div>
                                 <h1 className="mt-16 mb-20 flex justify-center text-center text-2xl">Do you prefer to play with your friends online? Then choose this!</h1>
                                 <div className="text-center">
-                                <button className="rounded-xl bg-black w-full py-2 text-white">Select gamemode</button>
+                                    <button className="rounded-xl bg-black w-full py-2 text-white">Select gamemode</button>
                                 </div>
                             </div>
-                            </Link>
-                        </div>
+                        </Link>
+                    </div>
                 </div>
-                </section>
+            </section>
         </>
 
     )
